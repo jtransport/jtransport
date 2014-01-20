93620a2dadcf16157475692c9b77c1329f8a9fa1
jtransport <vudinhkhai@gmail.com>
2014-01-20 11:48:06 +0700

    add plugin k2

---
+++ plugins/3rd_party/plg_jtransport_k2/schemas/joomla15/k2_items.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package     RedMIGRATOR.Backend
+ * @subpackage  Controller
+ *
+ * @copyright   Copyright (C) 2005 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * 
+ *  redMIGRATOR is based on JUpgradePRO made by Matias Aguirre
+ */
+
+class JTransportK2Item extends JTransport
+{
+    public function dataHook($rows)
+    {
+        foreach ($rows as &$row)
+        {
+            $row = (array) $row;
+            
+            $row['access'] = 1;
+        }
+
+        return $rows;
+    }
+}
+?>
